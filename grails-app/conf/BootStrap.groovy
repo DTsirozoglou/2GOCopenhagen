0d3ae295a19878846fdf7b9a1de02cf952baa7ae
DTsirozoglou <kjg920@alumni.ku.dk>
2015-01-17 08:41:00 +0100

    test

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -11,13 +11,13 @@ class BootStrap {
 
 	def init = { servletContext -> 
 		// Create roles and users for testing user system:
-//		def adminRole = new Role(authority: 'ROLE_ADMIN').save(failOnError: true, flush:true)
-//		
-//		def userRole = new Role(authority: 'ROLE_USER').save(failOnError: true, flush:true)
-//		
-//		def testUser = new User(password: 'password', username: "mail@mail.com", email: "mail@mail.com").save(failOnError: true, flush:true)
-//		//testUser.encodePassword()
-//		UserRole.create(testUser, adminRole, true)
+		def adminRole = new Role(authority: 'ROLE_ADMIN').save(failOnError: true, flush:true)
+		
+		def userRole = new Role(authority: 'ROLE_USER').save(failOnError: true, flush:true)
+		
+		def testUser = new User(password: 'password', username: "mail@mail.com", email: "mail@mail.com").save(failOnError: true, flush:true)
+		//testUser.encodePassword()
+		UserRole.create(testUser, adminRole, true)
 //		
 //		assert User.count() == 1
 //		assert Role.count() == 2
